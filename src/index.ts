e5f55bcc36e31bbf6277bc1d5721f619cdb0b710
marshamallow <hello@bwac.dev>
2021-04-16 18:08:51 +1000

    Calculate title

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,18 @@ const username = Deno.args[0]
 const baseUrl = "https://api.github.com"
 
 // Fetch user info from github.com
-const user = await octokitRequest.request(`get /users/${username}`, {baseUrl})
+const user = await octokitRequest.request(`get /users/${username}`, {baseUrl: baseUrl})
+
+// Create a title
+const title = `${Colors.blue(username)}@${Colors.blue(new URL(baseUrl).hostname)}`
+
+// Calculate barrier size for title
+let barrier = ""
+for (let index = 0; index < `${username}@${new URL(baseUrl).hostname}`.length; index++) {
+    barrier += `-`
+}
+
+console.log(title, barrier)
+
 
 console.log(`${username}@${new URL(baseUrl).hostname}`)
