93e6e843821076db776d386b08fe814e773ce522
marshamallow <hello@bwac.dev>
2021-04-17 13:19:26 +1000

    added command arguments

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 import * as Colors from 'https://deno.land/std/fmt/colors.ts'
 import * as octokitRequest from 'https://cdn.skypack.dev/@octokit/request?dts';
 import { getImageStrings } from "https://x.nest.land/terminal_images@3.0.0/mod.ts";
+import { Command } from "https://deno.land/x/cliffy/command/mod.ts";
 
-// Fetch from args
-const username = Deno.args[0]
+const { options } = await new Command()
+    .option("-u, --username [type:string]", "username of the user you want to ask")
+    .option("-b, --base-url [type:string]", "the base api url", { default: "https://api.github.com" })
+    .parse(Deno.args);
 
-let user
-
+    let user
 // Fetch user info from the internet
-if (username === undefined) {
+if (options.username === undefined) {
     // Try to grab from `gh` tool
     try {
         const p = Deno.run({cmd: ["gh", "api", "user"], stdout: "piped", stderr: "piped"})
@@ -32,29 +34,19 @@ if (username === undefined) {
             }
         } catch(err) {
             // They arent logged into GH
-            console.log(`${Colors.red("gh returned non-json.")}\n${Colors.yellow("Try simply passing your github username as the first argument")}`)
+            console.log(`${Colors.red("gh returned non-json.")}\n${Colors.yellow("Did you mean to use --username ?")}`)
             console.log(err)
             Deno.exit()
         }
     } catch(err) {
         // gh isn't installed
-        console.log(`${Colors.red("No username was given, and gh is not installed (or we dont have permission to use it)")}\n${Colors.yellow("Try passing your github username as the first argument")}`)
+        console.log(`${Colors.red("No username was given, and gh is not installed (or we dont have permission to use it)")}\n${Colors.yellow("Did you mean to use --username ?")}`)
         console.log(err)
         Deno.exit()
     }
 } else {
-    // Base url
-    let baseUrl
-    if (Deno.args[1] !== undefined) {
-        // They want a custom api
-        baseUrl = Deno.args[1]
-    } else {
-        // Assume they just want api.github.com
-        baseUrl = "https://api.github.com"
-    }
-
     // Use octokitRequest if username isn't given
-    const response = await octokitRequest.request(`get /users/${username}`, {baseUrl: baseUrl})
+    const response = await octokitRequest.request(`get /users/${options.username}`, {baseUrl: options.baseUrl})
     user = {
         Login: response.data.login,
         Blog: response.data.blog,
@@ -70,12 +62,12 @@ if (username === undefined) {
 const color = Colors.brightBlue
 
 // Create a title
-const title = `${color(user.Login)}@${color(new URL(baseUrl).hostname)}`
+const title = `${color(user.Login)}@${color(new URL(options.baseUrl).hostname)}`
 
 // Calculate barrier size for title
 let barrier = ""
 // Start at 1
-for (let index = 1; index < `${username}@${new URL(baseUrl).hostname}`.length; index++) {
+for (let index = 1; index < `${options.username}@${new URL(options.baseUrl).hostname}`.length; index++) {
     barrier += `-`
 }
 
