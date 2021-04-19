8edd17197584d00fdd2bbe58694669902f680b65
marshamallow <hello@bwac.dev>
2021-04-19 18:20:43 +1000

    added mkdir build

--- build.bash
+++ build.bash
@@ -1,3 +1,4 @@
+mkdir build
 deno compile --unstable -A --lite --quiet -o build/ghfetch-windows-x86_64 --target x86_64-pc-windows-msvc ghfetch.ts
 deno compile --unstable -A --lite --quiet -o build/ghfetch-linux-gnu-x86_64 --target x86_64-unknown-linux-gnu ghfetch.ts
 deno compile --unstable -A --lite --quiet -o build/ghfetch-darwin-x86_64 --target x86_64-apple-darwin ghfetch.ts
